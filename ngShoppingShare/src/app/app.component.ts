04af403177143aa8c8737a181cb18b9648354114
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-03-14 03:17:06 -0300

    Criação do projeto de Angular

---
+++ ngShoppingShare/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { SpinnerService } from './shared/spinner/spinner.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: [ 'app.component.scss' ]
+})
+export class AppComponent {
+
+
+
+}
