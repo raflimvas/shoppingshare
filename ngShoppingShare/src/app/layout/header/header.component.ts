0ddfb6fc0d36df6e3d0b5322e6e0e1fbf7cc17c1
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-03-24 20:54:28 -0300

    Ajustes no header

--- ngShoppingShare/src/app/layout/header/header.component.ts
+++ ngShoppingShare/src/app/layout/header/header.component.ts
@@ -1,25 +1,57 @@
-import { Component } from "@angular/core";
-import { UserSessionService } from "src/app/shared/services/usersession.service";
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { List, ListResult } from '@app/models/list.model';
+import { User } from '@app/models/user.model';
+import { faCog, faEdit, faPlusCircle, faSignOutAlt, faWrench } from '@fortawesome/free-solid-svg-icons';
+import { UserSessionService } from '@shared/services/usersession.service';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, map, take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
   styleUrls: [ 'header.component.scss' ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
-  constructor(private userSessionService: UserSessionService) { }
+  public editIcon = faEdit;
+  public plusIcon = faPlusCircle;
+  public wrenchIcon = faWrench;
+  public logoutIcon = faSignOutAlt;
+  public gearIcon = faCog;
+
+  public showProfile = false;
+
+  public userProfileData: User;
+  public lists: List[];
+
+  constructor(
+    private userSessionService: UserSessionService,
+    private headerService: HeaderService
+  ) { }
+
+  ngOnInit() {
+    this.headerService.getUser().subscribe(x => {
+      this.userProfileData = x;
+    });
+    this.headerService.getLists().subscribe(x => {
+      this.lists = x;
+    });
+  }
 
   public get isLoggedIn(): boolean {
     return this.userSessionService.isLoggedIn;
   }
 
-  public onLogoutClick(): void {
-
+  public get userProfilePicture(): any {
+    return this.userSessionService.getUserProfilePicture();
   }
 
-  public onLoginAuthenticated(event: any): void {
-
+  public onLogoutClick(): void {
+    this.userSessionService.clear();
+    window.location.reload();
   }
 
 }
---
+++ ngShoppingShare/src/app/layout/header/header.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { List, ListResult } from "@app/models/list.model";
+import { User } from "@app/models/user.model";
+import { UserSessionService } from "@app/shared/services/usersession.service";
+import { Observable, throwError } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { environment } from "src/environments/environment";
+
+@Injectable()
+export class HeaderService {
+
+  constructor(
+    private userSessionService: UserSessionService,
+    private http: HttpClient
+  ) { }
+
+  public getUser(): Observable<User> {
+    const id = this.userSessionService.getTokenSubject();
+    return this.http.get(
+      environment.apiEndpoint + 'user/' + id,
+      { observe: 'response' }
+    ).pipe(
+      map(res => new User(res.body)),
+      catchError(err => throwError(err))
+    );
+  }
+
+  public getLists(): Observable<List[]> {
+    return this.http.get(
+      environment.apiEndpoint + 'list/all',
+      { observe: 'response' }
+    ).pipe(
+      map(x => (new ListResult(x.body)).lists),
+      catchError(err => throwError(err))
+    );
+  }
+
+}
