e7d9992a0056f87c6b683628742e323993ced78f
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-03-25 10:28:53 -0300

    Otimização do modal do header

--- ngShoppingShare/src/app/layout/header/header.component.ts
+++ ngShoppingShare/src/app/layout/header/header.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { List, ListResult } from '@app/models/list.model';
 import { User } from '@app/models/user.model';
 import { faCog, faEdit, faPlusCircle, faSignOutAlt, faWrench } from '@fortawesome/free-solid-svg-icons';
 import { UserSessionService } from '@shared/services/usersession.service';
-import { Observable, of, throwError } from 'rxjs';
+import { forkJoin, Observable, of, throwError } from 'rxjs';
 import { catchError, map, take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { HeaderService } from './header.service';
@@ -16,29 +17,53 @@ import { HeaderService } from './header.service';
 })
 export class HeaderComponent implements OnInit {
 
+  private profileEnabled: boolean = false;
+  private profileReady: boolean = false;
+
   public editIcon = faEdit;
   public plusIcon = faPlusCircle;
   public wrenchIcon = faWrench;
   public logoutIcon = faSignOutAlt;
   public gearIcon = faCog;
 
-  public showProfile = false;
+  public get showProfile(): boolean {
+    return this.profileEnabled && this.profileReady;
+  };
 
-  public userProfileData: User;
-  public lists: List[];
+  public syncData: {user: User, lists: List[]};
+  public asyncData: Observable<{user: User, lists: List[]}>;
+
+  @ViewChild('profilemodal') public profileModal: ElementRef;
 
   constructor(
     private userSessionService: UserSessionService,
-    private headerService: HeaderService
-  ) { }
+    private headerService: HeaderService,
+    private router: Router,
+    private renderer: Renderer2
+  ) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (this.showProfile && this.profileModal?.nativeElement?.contains(e.target) === false) {
+        this.toogleProfile();
+      }
+    });
+  }
 
   ngOnInit() {
-    this.headerService.getUser().subscribe(x => {
-      this.userProfileData = x;
-    });
-    this.headerService.getLists().subscribe(x => {
-      this.lists = x;
-    });
+    this.asyncData = forkJoin([
+      this.headerService.getUser(),
+      this.headerService.getLists()
+    ]).pipe(map(x => { return { user: new User(x[0]) , lists: (x[1] ?? []).map(x => new List(x)) } }));
+  }
+
+  public toogleProfile(): void {
+    this.profileReady = false;
+    this.profileEnabled = !this.profileEnabled;
+    if (this.profileEnabled) {
+      this.asyncData.subscribe(x => {
+        this.syncData = x;
+        this.profileReady = true;
+      });
+    }
   }
 
   public get isLoggedIn(): boolean {
@@ -52,6 +77,18 @@ export class HeaderComponent implements OnInit {
   public onLogoutClick(): void {
     this.userSessionService.clear();
     window.location.reload();
+  }
+
+  onEdit(e: number) {
+    this.toogleProfile();
+    history.pushState({ id_list: e }, '', '/member/updatelist')
+    this.router.navigate(['member', 'updatelist'], { skipLocationChange: true });
+  }
+
+  onConfig(e) {
+    this.toogleProfile();
+    history.pushState({ id_list: e }, '', '/member/configlist')
+    this.router.navigate(['member', 'configlist'], { skipLocationChange: true });
   }
 
 }
