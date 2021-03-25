915abf86ef952d3d54a1178054cece440abe4925
Gabriel Pupim <gabriel.pda15@gmail.com>
2021-03-25 16:04:43 -0300

    Ajustes para heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/ngShoopingShare`));
+app.use(express.static(`${__dirname}/dist/ngShoppingShare`));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/ngShoopingShare/index.html`));
+  res.sendFile(`${__dirname}/dist/ngShoppingShare/index.html`);
 });
 
 app.listen(process.env.PORT || 8080);
